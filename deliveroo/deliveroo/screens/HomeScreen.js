ae8c7e39a850a1c50421a6347ac9807fc13faa0c
vanshaj_aqua <vanshaj.dhar@aquasec.com>
2024-02-29 18:24:31 +0530

    First commit

---
+++ deliveroo/deliveroo/screens/HomeScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const HomeScreen = () => {
+    return (
+        <View>
+        <Text className="text-red-600">
+                This is home screen
+            </Text>
+        </View>
+    )
+};
+
+export default HomeScreen;
